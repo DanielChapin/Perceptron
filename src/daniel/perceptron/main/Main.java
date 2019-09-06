659b91f40a684d44b36e89e4d546ac343f7dc1a3
Daniel Chapin <dchapes123@gmail.com>
2019-09-06 12:32:25 -0500

    Begun adding user input and setup JFrame

--- src/daniel/perceptron/main/Main.java
+++ src/daniel/perceptron/main/Main.java
@@ -1,9 +1,18 @@
 package daniel.perceptron.main;
 
-public class Main {
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
 	
 	public static void main(String[] args) {
-		
+		new Main();
+	}
+	
+	Main() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(1200, 800);
+		setLocationRelativeTo(null);
+		setVisible(true);
 	}
 	
 }
