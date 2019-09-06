77d0413d0b3d8fcf0d87cf63deeb7496af93652e
Daniel Chapin <dchapes123@gmail.com>
2019-09-06 12:33:10 -0500

    Added basic user input

--- src/daniel/perceptron/main/Main.java
+++ src/daniel/perceptron/main/Main.java
@@ -1,18 +1,58 @@
 package daniel.perceptron.main;
 
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JFrame;
 
-public class Main extends JFrame {
+public class Main extends JFrame implements MouseListener {
+	
+	int pointIndex = 0;
+	Point[] points = new Point[2];
 	
 	public static void main(String[] args) {
 		new Main();
 	}
 	
 	Main() {
+		// Setup the JFrame
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(1200, 800);
 		setLocationRelativeTo(null);
+		setTitle("Machine Learning Basics");
 		setVisible(true);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// Get the location of the mouse press when the user releases the mouse
+		Point pressLocation = e.getPoint();
+		// Check if the slope of the line is undefined, otherwise set the points.
+		if (pointIndex == 1 && pressLocation.x == points[0].x) {
+			System.out.println("Undefined Slope!");
+		} else {
+			// Set either the first or second point to the press location depending on the last one set.
+			points[pointIndex] = pressLocation;
+			// Flip the bit. (if its 1 set it to 0 and if its 0 set it to 1)
+			pointIndex = ~pointIndex;
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
 	}
 	
 }
