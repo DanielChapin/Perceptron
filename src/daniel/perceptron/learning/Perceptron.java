ca8b3b86966e01b0e2ee46dd59eae3f5d3e0d64f
Daniel Chapin <dchapes123@gmail.com>
2019-09-10 12:31:17 -0500

    Began to document Perceptron

--- src/daniel/perceptron/learning/Perceptron.java
+++ src/daniel/perceptron/learning/Perceptron.java
@@ -4,9 +4,11 @@ public class Perceptron {
 	
 	float[] weights;
 	
+	// A modifier to make sure that it doesn't overshoot its goal
 	float learningRate = 0.01f;
 	
 	public Perceptron(int numInputs) {
+		// Initialize the weights
 		weights = new float[numInputs];
 		// Generate random values for all the weights at the beginning
 		for (int i = 0; i < weights.length; i++)
@@ -14,20 +16,28 @@ public class Perceptron {
 	}
 	
 	public void train(int[] inputs, int slope) {
+		// Get a guess
 		int guess = guess(inputs);
+		// Calculate the error
 		int error = slope - guess;
+		// Adjust the weights based on the error and how off they were
 		for (int i = 0; i < weights.length; i++)
 			weights[i] += error * inputs[i] * learningRate;
 	}
 	
 	public int guess(int[] inputs) {
+		// init a var for the weighted sum
 		float sum = 0;
+		// Calculate the weighted sum
 		for (int i = 0; i < inputs.length; i++)
 			sum += inputs[i] * weights[i];
+		// Return the activated sum
 		return activate(sum);
 	}
 	
+	// The activation function: turns the weighted sum into an output
 	int activate(float sum) {
+		// Checks if the sum is greater than or equal to 0 or less than 0
 		return (sum >= 0) ? 1 : -1;
 	}
 
